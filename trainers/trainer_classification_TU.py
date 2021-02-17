6d7e1d1910cae7866a28b4f71909f15187ff9b10
JinheonBaek <Jinheon.Baek@outlook.kr>
2021-02-17 14:37:54 +0900

    (feat): implement parsers, trainers, and main structures

---
+++ trainers/trainer_classification_TU.py
@@ -0,0 +1,28 @@
+import os
+import time
+
+class Trainer(object):
+
+    def __init__(self, args):
+
+        super(Trainer, self).__init__()
+
+    def load_data(self):
+
+        pass
+
+    def load_model(self):
+
+        pass
+
+    def train(self):
+
+        pass
+
+    def eval(self, loader):
+
+        pass
+
+    def experiment_name(self, args):
+
+        pass
